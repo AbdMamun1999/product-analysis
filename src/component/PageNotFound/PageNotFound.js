2f9521a4c4acae62b40785a06179e23f5f629ff8
AbdMamun1999 <mamunrox@gmail.com>
2022-04-05 23:57:58 +0600

    add page not found style

--- src/component/PageNotFound/PageNotFound.js
+++ src/component/PageNotFound/PageNotFound.js
@@ -3,7 +3,8 @@ import React from 'react';
 const PageNotFound = () => {
     return (
         <div>
-            <h1>Page NOT Found</h1>
+            <h1 className='text-center font-2xl'>Page NOT Found</h1>
+            <p className='text-center font-2xl'>404</p>
         </div>
     );
 };
